376e52b5d900cc737368fdba598d89ee8e6b1b0c
Braulio <braulio@Mac-Pro-de-Braulio.local>
2014-10-15 22:08:22 -0500

    Initial Commit

---
+++ Homework-3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework-3
+//
+//  Created by Braulio on 15/10/14.
+//  Copyright (c) 2014 utzmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homework-3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Homework-3
+//
+//  Created by Braulio on 15/10/14.
+//  Copyright (c) 2014 utzmg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
